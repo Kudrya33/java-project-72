ebc98c2bad9d314dc006817b7620cb2b98bef47d
Павел Кудря <Skelger@mail.ru>
2025-03-21 15:06:22 +0300

    update project

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import java.io.IOException;
+import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+        return app;
+    }
+}
